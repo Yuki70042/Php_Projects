eb5d8c322b7999f7d553cb45999c722da6e5b198
Yuki70042 <lizlink2004@gmail.com>
2024-09-09 13:19:25 +0200

    Commencement exo 3

---
+++ 00.Cours/Decouverte php/Exercice 03_CINOCHE/Ex03_CINOCHE_Demande.php
@@ -0,0 +1,29 @@
+<html>
+
+    <head>
+
+    </head>
+
+
+    <body>
+
+    <h1>Cinoche</h1>
+
+    <h3>Tarif</h3>
+    <form method="post" action="Ex03_CINOCHE_Reponse.php">
+        Type de ticket : <input type="radio" name="type_tarif" value="1" checked>  Plein Tarif : 8,70 Euros <br>
+        Type de ticket : <input type="radio" name="type_tarif" value="2" >  Etudiant ou minuer : 5,90 Euros <br>
+        Type de ticket : <input type="radio" name="type_tarif" value="3" >  Tarif reduit autre : 6,90 euros <br>
+        Type de ticket : <input type="radio" name="type_tarif" value="4" >  Tarif special CE : 5,20 Euros <br>
+
+        <br>
+
+        <input type="checkbox" name="3D_Film"> Majoration Film 3D : 1,50 Euro <br>
+        <input type="checkbox" name="3D_Film"> Coupon reduction : - 1 Euro <br>
+
+    </form>
+
+
+    </body>
+
+</html>
