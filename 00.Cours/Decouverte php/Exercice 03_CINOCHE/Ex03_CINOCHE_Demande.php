dfdebd5130d7a4fae6c66c6e1c59749e53b61d33
Yuki70042 <lizlink2004@gmail.com>
2024-09-10 10:28:28 +0200

    Realisation du 3eme exercice

--- 00.Cours/Decouverte php/Exercice 03_CINOCHE/Ex03_CINOCHE_Demande.php
+++ 00.Cours/Decouverte php/Exercice 03_CINOCHE/Ex03_CINOCHE_Demande.php
@@ -9,18 +9,37 @@
 
     <h1>Cinoche</h1>
 
-    <h3>Tarif</h3>
+    <h4>Tarif</h4>
     <form method="post" action="Ex03_CINOCHE_Reponse.php">
+        Jour de la semaine : <select name="jour_semaine">
+            <option value="1">Lundi</option>
+            <option value="2">Mardi</option>
+            <option value="3">Mercredi</option>
+            <option value="4">Jeudi</option>
+            <option value="5">Vendredi</option>
+            <option value="6">Samedi</option>
+            <option value="7">Dimanche</option>
+        </select>
+
+        <br> <br>
+
+        Heure : <input type="time" name="time">
+
+        <br> <br>
+
         Type de ticket : <input type="radio" name="type_tarif" value="1" checked>  Plein Tarif : 8,70 Euros <br>
-        Type de ticket : <input type="radio" name="type_tarif" value="2" >  Etudiant ou minuer : 5,90 Euros <br>
+        Type de ticket : <input type="radio" name="type_tarif" value="2" >  Etudiant ou mineur : 5,90 Euros <br>
         Type de ticket : <input type="radio" name="type_tarif" value="3" >  Tarif reduit autre : 6,90 euros <br>
         Type de ticket : <input type="radio" name="type_tarif" value="4" >  Tarif special CE : 5,20 Euros <br>
 
         <br>
 
-        <input type="checkbox" name="3D_Film"> Majoration Film 3D : 1,50 Euro <br>
-        <input type="checkbox" name="3D_Film"> Coupon reduction : - 1 Euro <br>
+        <input type="checkbox" name="Film_3D" value="une checkbox"> Majoration Film 3D : 1,50 Euro <br>
+        <input type="checkbox" name="Coupon" value="une seconde checkox"> Coupon reduction : - 1 Euro <br>
 
+            <br>
+
+        <input type="submit" name="Price_button" value="Decouvrir le prix de ma place">
     </form>
 
 
--- 00.Cours/Decouverte php/Exercice 03_CINOCHE/Ex03_CINOCHE_Reponse.php
+++ 00.Cours/Decouverte php/Exercice 03_CINOCHE/Ex03_CINOCHE_Reponse.php
@@ -1,1 +1,63 @@
 <?php
+
+// On declare en php les variables envoyer par le client
+$jour_semaine = $_POST["jour_semaine"];
+$type_tarif = $_POST["type_tarif"];
+$time = $_POST["time"];
+
+if (isset($_POST["Film_3D"])){
+    $Film_3D = true;
+}
+else{
+    $Film_3D = false;
+}
+
+if (isset($_POST["Coupon"])){
+    $Coupon = true;
+}
+else{
+    $Coupon = false;
+}
+
+
+
+
+if ($jour_semaine == "1" || $time == "11:10"){
+    $type_tarif = "4";
+}
+
+
+
+switch ($type_tarif) {
+    case '1':
+        $prix = 8.70;
+        break;
+    case '2':
+        $prix = 5.90;
+        break;
+    case '3':
+        $prix = 6.90;
+        break;
+    case '4':
+        $prix = 5.20;
+        break;
+    default:
+        throw new Exception("Erreur d'un type de tarif");
+}
+
+if ($Coupon && $type_tarif != "4" ){
+    $prix -= 1;
+}
+elseif ($Film_3D){
+    $prix += 1.50;
+}
+
+
+
+
+
+
+
+
+echo "Vous payerez votre place: ", $prix, " euros";
+
